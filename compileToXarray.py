ee9828e0278d0dd1014b73383455b8d3cd09980d
HaynesStephens <hfstephens13@gmail.com>
2022-02-09 16:37:37 -0800

    setup for saving files for Phase 2

--- compileToXarray.py
+++ compileToXarray.py
@@ -7,6 +7,7 @@ climate_phase2 = ['AgMERRA', 'AgMERRA', 'AgMERRA', 'AgMERRA', 'ERAI']
 model, climate = models_phase2[0], climate_phase2[0]
 load_path = '/project2/ggcmi/AgMIP.output/{0}/{1}/maize/A0/'.format(model, phase)
 
+# LOAD DATASET WITH ALL VARIABLES FOR A GIVEN SCENARIO 
 vars = ['yield', 'plant-day', 'maty-day', 'trzpah2o', 'runoff']
 shift_coord = 'T'
 shift_val = 0
@@ -14,6 +15,13 @@ file_paths = [load_path + '{0}/{1}_{2}_fullharm_{0}_mai_global_annual_1980_2010_
 ds = xr.merge([xr.open_dataset(name, decode_times=False) for name in file_paths])
 ds['time'] = pd.date_range(start='12-31-1980', periods=31, freq='A')
 ds = ds.sel(lat=slice(48.75, 36.25), lon=slice(-103.8, -80.75), time=slice('1981', '2010'))
+
+# SAVE DATASET TO SEPARATE DIRECTORY
+save_name = '{0}_{1}_cornbelt_1981_2010_C360_T{2}_W0_N200_A0.nc4'.format(model.lower(), climate.lower(), shift_val)
+
+# LOOP THROUGH MODELS AND SCENARIOS
+
+# THEN GO ON TO PHASE 3
 
 
 
