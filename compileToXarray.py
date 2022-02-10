9b497fb340c91ac4a215dace424bf2cba7271df5
HaynesStephens <hfstephens13@gmail.com>
2022-02-09 16:12:08 -0800

    error edit

--- compileToXarray.py
+++ compileToXarray.py
@@ -9,7 +9,7 @@ load_path = '/project2/ggcmi/AgMIP.output/{0}/{1}/maize/A0/'.format(model, phase
 var = 'yield'
 shift_coord = 'T'
 shift_range = [0, 2, 4, 6]
-file_path = load_path + '{0}_{1}_fullharm_{2}_mai_global_annual_1980_2010_C360_T*_W0_N200_A0.nc4'.format(model.lower(), climate.lower(), var)
+file_path = load_path + '{0}/{1}_{2}_fullharm_{0}_mai_global_annual_1980_2010_C360_T*_W0_N200_A0.nc4'.format(var, model.lower(), climate.lower())
 
 ds = xr.openmfdataset(file_path)
 
