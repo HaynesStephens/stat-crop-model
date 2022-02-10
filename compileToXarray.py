b4fa75f3850704f0ec6b6942557f79126cd0bf25
HaynesStephens <hfstephens13@gmail.com>
2022-02-09 16:22:14 -0800

    loop

--- compileToXarray.py
+++ compileToXarray.py
@@ -7,11 +7,11 @@ climate_phase2 = ['AgMERRA', 'AgMERRA', 'AgMERRA', 'AgMERRA', 'ERAI']
 model, climate = models_phase2[0], climate_phase2[0]
 load_path = '/project2/ggcmi/AgMIP.output/{0}/{1}/maize/A0/'.format(model, phase)
 
-var = 'yield'
+vars = ['yield', 'plant-day', 'maty-day', 'trzpah2o', 'runoff']
 shift_coord = 'T'
 shift_val = 0
-file_path = load_path + '{0}/{1}_{2}_fullharm_{0}_mai_global_annual_1980_2010_C360_T{3}_W0_N200_A0.nc4'.format(var, model.lower(), climate.lower(), shift_val)
-ds = xr.load_dataset(file_path, decode_times=False)
+file_paths = [load_path + '{0}/{1}_{2}_fullharm_{0}_mai_global_annual_1980_2010_C360_T{3}_W0_N200_A0.nc4'.format(var, model.lower(), climate.lower(), shift_val) for var in vars]
+ds = xr.merge[xr.open_dataset(name, decode_times=False) for name in file_paths]
 
 
 
