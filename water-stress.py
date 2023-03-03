32c8f1b46cc09a0c789cb91e413b25d5cc79e883
HaynesStephens <hfstephens13@gmail.com>
2023-03-03 11:19:34 -0800

    test

--- water-stress.py
+++ water-stress.py
@@ -7,6 +7,6 @@ var = 'transp'
 T = 0
 basedir = '/project2/ggcmi/AgMIP.output/{0}/phase2/maize/A0/{1}/'.format(model,var)
 winf = '{0}_agmerra_fullharm_{1}_mai_global_annual_1980_2010_C360_T{2}_Winf_N200_A0.nc4'.format(model.lower(), var, T)
-winf = xr.open_dataarray( basedir + winf )
+winf = xr.open_dataarray( basedir + winf , decode_times=False)
 w0 = '{0}_agmerra_fullharm_{1}_mai_global_annual_1980_2010_C360_T{2}_W0_N200_A0.nc4'.format(model.lower(), var, T)
-w0 = xr.open_dataarray( basedir + w0 )
+w0 = xr.open_dataarray( basedir + w0 , decode_times=False)
